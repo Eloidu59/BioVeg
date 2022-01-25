75376b8cf5af89bbaeecf6fdb8bec084ed297828
Manon000 <65402358+Manon000@users.noreply.github.com>
2022-01-25 19:56:19 +0100

    navigation

--- views/js/function.js
+++ views/js/function.js
@@ -10,17 +10,42 @@ function toggleSidebar() {
     this.mini = true;
   }
 }
-let dropped = true
-document.getElementById("dropCtn").style.display="none"
 
-document.getElementById("dropBtn").addEventListener('click',()=>{
-    console.log("j'ai cliqué sur le boutoooooon")
-    if(dropped){
-        document.getElementById("dropCtn").style.display="block"
-        dropped = false
-    }
-    else{
-        document.getElementById("dropCtn").style.display="none"
-        dropped = true
-    }
+//Angiospermes
+document.getElementById("dropAngioCtn").style.display="none"
+document.getElementById("dropAngio").addEventListener('mouseover',()=>{
+  document.getElementById("dropAngioCtn").style.display="block"
 })
+document.getElementById("dropAngio").addEventListener('mouseleave',()=>{
+  document.getElementById("dropAngioCtn").style.display="none"
+});
+
+
+//GymnoSpermes
+document.getElementById("dropGymnoCtn").style.display="none"
+document.getElementById("dropGymno").addEventListener('mouseover',()=>{
+  document.getElementById("dropGymnoCtn").style.display="block"
+})
+document.getElementById("dropGymno").addEventListener('mouseleave',()=>{
+  document.getElementById("dropGymnoCtn").style.display="none"
+});
+
+
+//Fougeres
+document.getElementById("dropFougCtn").style.display="none"
+document.getElementById("dropFoug").addEventListener('mouseover',()=>{
+  document.getElementById("dropFougCtn").style.display="block"
+})
+document.getElementById("dropFoug").addEventListener('mouseleave',()=>{
+  document.getElementById("dropFougCtn").style.display="none"
+});
+
+
+//Annales
+document.getElementById("dropAnCtn").style.display="none"
+document.getElementById("dropAn").addEventListener('mouseover',()=>{
+  document.getElementById("dropAnCtn").style.display="block"
+})
+document.getElementById("dropAn").addEventListener('mouseleave',()=>{
+  document.getElementById("dropAnCtn").style.display="none"
+});
